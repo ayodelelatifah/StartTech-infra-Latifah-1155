befec46760c1aeab62d091028ac2b6b381dbf77a
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 17:04:58 +0100

    oui

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,19 @@
-output "backend_url" {
-  value = module.app.alb_dns_name
+output "frontend_bucket_id" {
+  value       = aws_s3_bucket.frontend.id
+  description = "The name of the S3 bucket used for frontend hosting"
 }
 
-output "frontend_url" {
-  value = module.app.cloudfront_domain
+output "redis_endpoint" {
+  value       = aws_elasticache_cluster.redis.cache_nodes[0].address
+  description = "The connection endpoint for the Redis cluster"
+}
+
+output "ecr_repository_url" {
+  value       = aws_ecr_repository.backend.repository_url
+  description = "The URL of the ECR repository for Docker images"
+}
+
+output "alb_dns_name" {
+  value       = aws_alb.main.dns_name
+  description = "The DNS name of the Load Balancer"
 }
