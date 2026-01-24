da277f0d2278d830a5befda924fdb6548d5224e2
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 20:56:37 +0100

    fixed

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,5 +1,11 @@
 resource "aws_elasticache_cluster" "redis" {
-  # ...same resource config...
+  cluster_id           = "starttech-redis-v5"
+  engine               = "redis"
+  node_type            = "cache.t3.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  port                 = 6379
+
   lifecycle {
     prevent_destroy = true
   }
