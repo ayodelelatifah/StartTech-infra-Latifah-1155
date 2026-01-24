12acc2d46ca5a505562290257b2e921bf50dcd5d
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 09:36:41 +0100

    main

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 # This block pulls in the code you wrote in modules/networking
 module "networking" {
   source              = "./modules/networking"
