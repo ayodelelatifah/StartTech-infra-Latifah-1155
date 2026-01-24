830c92964411ddb787b9313ad11299fae67806d2
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 10:08:00 +0100

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,17 @@
-# Root main.tf
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# Root main.tf 
+# Note: Terraform and Provider blocks are now only in provider.tf
 
 # This block pulls in the code you wrote in modules/networking
 module "networking" {
-  source              = "./modules/networking"
-  vpc_cidr            = "10.0.0.0/16"
-  public_subnet_cidr  = "10.0.1.0/24"
+  source             = "./modules/networking"
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnet_cidr = "10.1.0.0/24"
 }
 
+# This block pulls in the code you wrote in modules/app
 module "app" {
   source     = "./modules/app"
-  vpc_id     = module.networking.vpc_id # Uses the output we created!
-  subnet_id  = module.networking.public_subnet_id
+  vpc_id     = module.networking.vpc_id
+  # Updated to plural 'subnet_ids' to match your variables.tf fix
+  subnet_ids = module.networking.public_subnet_ids 
 }
