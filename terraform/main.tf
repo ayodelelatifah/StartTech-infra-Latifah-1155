83ca4d277ae9d1d73f5af7b3c5b44af1e6593b03
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 11:23:33 +0100

    yes

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@
 module "networking" {
   source             = "./modules/networking"
   vpc_cidr           = "10.0.0.0/16"
-  public_subnet_cidr = "10.1.0.0/24"
+  public_subnet_cidr = "10.0.1.0/24"
 }
 
 module "app" {
