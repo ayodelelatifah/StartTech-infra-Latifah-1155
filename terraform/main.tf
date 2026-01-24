468becded254931ab30836894c30c625c1eb06ce
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 06:00:36 +0100

    Initial commit: Infrastructure and Application structure

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+# Root main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# This block pulls in the code you wrote in modules/networking
+module "networking" {
+  source              = "./modules/networking"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+}
---
+++ terraform/modules/networking/main.tf
@@ -0,0 +1,41 @@
+# Create the VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = "starttech-vpc"
+  }
+}
+
+# Create a Public Subnet for the Frontend
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "starttech-public-subnet"
+  }
+}
+
+# Create an Internet Gateway to allow internet access
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "starttech-igw"
+  }
+}
+
+# Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
