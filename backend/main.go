4733d4efa78abc607ceb3ec2372ae49ad761d8ec
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 08:25:48 +0100

    added a file

--- backend/main.go
+++ backend/main.go
@@ -1,2 +1,13 @@
- 
-// trigger build 
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, StartTech!")
+	})
+	http.ListenAndServe(":8080", nil)
+} 
