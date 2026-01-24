1b4b0be77f59f0871ed00b6acab9e5680d3b12fe
Oyeniyi Latifah <ayodelelatifah@gmail.com>
2026-01-24 10:02:38 +0100

    tired face

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,8 +1,9 @@
 variable "vpc_id" {
+  type        = string
   description = "The ID of the VPC from networking module"
 }
 
-variable "subnet_ids" { # Change this to plural
-  type        = list(string) # Add this type
+variable "subnet_ids" { # Note the 's' for plural
+  type        = list(string) # This MUST be list(string)
   description = "The list of public subnets from networking module"
 }
